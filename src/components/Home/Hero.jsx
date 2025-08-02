7d6ce0cb19b9c4a4cef8259176e29b15e507b7a0
AasthaOswal <aasthaoswal29@gmail.com>
2025-08-02 10:01:49 +0530

    fix hero section

--- src/components/Home/Hero.jsx
+++ src/components/Home/Hero.jsx
@@ -2,25 +2,25 @@
 const Hero=()=>{
     return (
         <section className="bg-slate-950 text-white flex items-center justify-center px-6 py-16 scroll-mt-16" id="hero">
-            <div className="flex max-w-5xl items-center justify-around gap-x-8">
-                <div className=" text-left">
-                    <h1 className="text-4xl md:text-5xl font-bold mb-6">
+            <div className="flex max-w-5xl flex-col-reverse  md:flex-row items-center justify-around gap-x-8">
+                <div className="text-center md:text-left mt-16 md:mt-0">
+                    <h1 className="text-2xl sm:text-3xl md:text-4xl font-bold mb-6">
                     CSI-DMCE: Ignite. Innovate. Inspire.
                     </h1>
-                    <p className="text-lg md:text-xl mb-8 text-slate-300">
+                    <p className="text-md md:text-base mb-8 text-slate-300">
                     Empowering developers, designers, and dreamers at Datta Meghe College of Engineering through
                     collaborative projects, hands-on learning, and tech-driven innovation.
                     </p>
-                    <div className="flex flex-col sm:flex-row gap-4 justify-start">
-                        <button className="bg-teal-400 text-slate-900 font-semibold px-6 py-3 rounded-xl hover:bg-teal-300 transition">
+                    <div className="flex flex-row flex-wrap md:flex-row gap-4 justify-center md:justify-start">
+                        <button className="bg-teal-400 text-slate-900 font-semibold px-6 py-3 rounded-xl hover:bg-teal-300 transition w-fit self-center">
                             <a href="#events">Explore Events</a>
                         </button>
-                        <button className="border border-teal-400 text-teal-400 px-6 py-3 rounded-xl hover:bg-teal-400 hover:text-slate-900 transition">
+                        <button className="border border-teal-400 text-teal-400 px-6 py-3 rounded-xl hover:bg-teal-400 hover:text-slate-900 transition w-fit self-center">
                             Join Our Community
                         </button>
                     </div>
                 </div>
-                <img src="/hero-image.png" alt="" className="w-2/5 "/>
+                <img src="/hero-image.png" alt="" className="w-11/12 sm:w-3/5 md:w-2/5 "/>
             </div>
         </section>
 
